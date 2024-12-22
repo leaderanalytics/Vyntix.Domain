a8b57b6ddd646ffe23967f023a95ab955fcca105
Sam Wheat <sam.wheat@outlook.com>
2024-12-22 12:43:33 -0800

    Update IconURIBase constant

--- Vyntix.Domain/Components/Constants.cs
+++ Vyntix.Domain/Components/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string IconURIBase = @"/LeaderAnalytics.FF.Vintages.Components;component/Resources/Icons/";
+    public const string IconURIBase = @"/LeaderAnalytics.Vyntix.Components;component/Resources/Icons/";
     public const string DataProviderIconFileNameBase = @"Resources\DataProviderIcons\";
     public const string IdentityScope = "IdentityScope";
 }
